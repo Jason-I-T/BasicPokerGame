178b25e57b122548103591c3be3ec966f8bc4001
jason-I-T <j.istejada29@gmail.com>
2022-10-17 10:44:04 -0700

    Started on implementing card dealing

---
+++ src/Dealer.java
@@ -0,0 +1,17 @@
+/*
+ * jason-I-T ... Basic Poker Game
+ *
+ * Desc: This program describes the dealer. Data structure of 52 elements for
+ * deck. Operations to deal cards.
+ *
+ */
+
+public class Dealer {
+	public Dealer() {
+		//TODO:
+		/* Create the deck (4 suits, A, 2-10, J, Q, K)
+		 * Implement a shuffle
+		 * Implement the card dealing
+		 */	
+	}
+}
