ca055f7203845a0951de404b1351fa2540f7e554
jason-I-T <j.istejada29@gmail.com>
2022-10-18 17:44:14 -0700

    Implemented a way to ensure the game deals unique cards

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,21 +1,22 @@
 /*
- * jason-I-T ... Basic Poker Game
+ * jason-I-T ... Basic Poker Game... Dealer.java
  *
- * Desc: This program describes the dealer. Data structure of 52 elements for
- * deck. Operations to deal cards.
- *
+ * Desc: This program describes the dealer. Data structure of 15 elements for
+ * the deck. The deck is used in operations to deal cards.
  */
+
 import java.util.HashMap;
+import java.util.Collection;
+import java.util.ArrayList;
 import java.lang.Integer;
-//import java.lang.Math;
 
 public class Dealer {
 	private HashMap<Integer, int[]> deck;
 
 	public Dealer() {
-		//TODO:
-		/* Redo creating the cards we play with
-		 * Implement the card dealing
+		/*TODO:
+		 * 	Implement dealing to players and the river
+		 * 	Implement a way to reveal the river after each stage	
 		 */	
 		this.deck = deckInit();
 	}
@@ -23,13 +24,52 @@ public class Dealer {
 	// Create new deck
 	private HashMap<Integer, int[]> deckInit() {
 		HashMap<Integer, int[]> cards = new HashMap<Integer, int[]>();
-		for(int i = 0; i < 15; i++) {
+		
+		// Uniquely picking a random cards until we have 15 (hand+river)
+		int i = 0;
+		do { 
 			int suit = (int)((Math.random() * 1000) % 4);
 			int rank = (int)((Math.random() * 1000) % 13);	
-			cards.put(i, new int[] {suit, rank});
-		}	
+			int[] temp = {suit, rank};
+			//If card doesn't exist add to map
+			if(!isCopy(cards, temp)) {
+				cards.put(i, temp);	
+				i++;
+			} 
+		}while(i < 15);	
+
 		return cards;
 	}
+
+	/*
+	private Stack<Integer> handInit(HashMap<Integer, int[]> deck) {
+		
+	}
+
+	private Queue<Integer> riverInit() {
+	
+	}
+	*/
+
+	// Used to ensure the deck will not have copies...deckInit()
+	private boolean isCopy(HashMap<Integer, int[]> map, int[] newEntry){
+		Collection<int[]> vals = map.values();
+		ArrayList<int[]> valuePairs = new ArrayList<int[]>(vals);
+
+		for(int i = 0; i < valuePairs.size(); i++) {
+			int[] oldEntry = valuePairs.get(i);
+			if(oldEntry[0]==newEntry[0] && oldEntry[1]==newEntry[1])
+				return true;	
+		}
+
+		return false;	
+	}
+
+	/**********************************************************************
+	 **********************************************************************
+	 **********************************************************************
+	 **********************************************************************
+	 **********************************************************************/
 
 	public void printDeck() {
 		this.deck.forEach((n, c) -> {
