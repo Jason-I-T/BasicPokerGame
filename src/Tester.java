9d41feb9f87d8cdd914387e2de223b7efcb6cd93
jason-I-T <j.istejada29@gmail.com>
2022-10-28 10:01:24 -0700

    Added testing for card dealing implementation

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Tester{
 	public static void main(String[] args) {
 		Dealer d = new Dealer();
-		d.printDeck();
-		d.printHandCards();
-		d.printRiverCards();
+		//d.printDeck();
+		//d.printHandCards();
+		//d.printRiverCards();
+
+		ArrayList<LinkedList<int[]>> testHands = d.getPlayerHands(); 
+		for(int i = 0; i < 5; i++) {
+			LinkedList<int[]> oneHand = testHands.get(i);	
+			oneHand.forEach((c) -> {
+				System.out.printf("..%d %d..", c[0], c[1]);	
+			});
+			System.out.println("\n\n");
+		}
 	}
 }
