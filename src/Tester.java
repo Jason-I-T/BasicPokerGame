3f1513299a251c19f4f84b44ef165c0fb17481e2
jason-I-T <j.istejada29@gmail.com>
2022-10-29 10:06:06 -0700

    Added testing for dealer river implementation

--- src/Tester.java
+++ src/Tester.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 public class Tester{
 	public static void main(String[] args) {
 		Dealer d = new Dealer();
-		//d.printDeck();
+		d.printDeck();
 		//d.printHandCards();
 		//d.printRiverCards();
 
@@ -15,6 +15,19 @@ public class Tester{
 				System.out.printf("..%d %d..", c[0], c[1]);	
 			});
 			System.out.println("\n\n");
+		}
+
+		for(int i = 0; i < 3; i++) { // 3 stages of river revealing..
+			int[] a;
+			if(i == 0) {
+				for(int j = 0; j < 3; j++) {
+					a = d.getRiver();
+					System.out.println(a[0] + ",, " + a[1]);
+				}	
+				continue;
+			}
+			a = d.getRiver();
+			System.out.println(a[0] + ", " + a[1]);	
 		}
 	}
 }
